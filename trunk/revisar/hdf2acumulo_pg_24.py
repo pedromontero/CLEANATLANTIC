37e9bf414090a0b6a20cbf9e40eb83986a2a6a99
Pedro Montero <pmontero@intecmar.gal>
2020-12-04 14:49:45 +0100

    Input from a json file. Clean up folders

--- trunk/revisar/hdf2acumulo_pg_24.py
+++ trunk/revisar/hdf2acumulo_pg_24.py
@@ -207,28 +207,7 @@ def main():
                                  , hour=int(time[3]),minute= int(time[4]), second=int(time[5]))
         inv_dates[data] = root_name_time
 
- 
-        
 
-
-
-
-
-    #a_group_key = times_group_list[len(times_group_list)-1]
-    #last_time_list = [a_group_key]
-    #print(last_time_list)
-
-    #name_time = last_time_list[0]
-    #print(name_time)
-    #num_name = name_time.split('_')[1]
-    #val_num_name = int(num_name)
-    #root_name_time = name_times_group + name_time
-    #time = f[root_name_time]
-    #name_data = '%04d/%02d/%02dT%02d:%02d:%02d' % \
-    #            (int(time[0]), int(time[1]), int(time[2]), int(time[3]), int(time[4]), int(time[5]))
-    #data = datetime.datetime(year=int(time[0]),month= int(time[1]), day=int(time[2])
-     #                        , hour=int(time[3]),minute= int(time[4]), second=int(time[5]))
-    
     # tempos.
      
     for n in range(0, len(datas_list)):

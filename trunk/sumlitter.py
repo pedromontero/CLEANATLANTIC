25f20312a83fb897cd9dd513448f0ec46f1cf78b
Pedro Montero <pmontero@intecmar.gal>
2020-12-15 07:01:53 +0100

    conexion() uses a json file to receive the parameters connections

--- trunk/sumlitter.py
+++ trunk/sumlitter.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-**hdflitter_agrega.py**
+**sumlitter.py**
 
 * *Purpose:* Crea nuevos registros de acumulación, pero ahora a partir de los anteriores porque agrega días. Ejemplo:
 De los acumulos de 24 horas, crea otra lista de acumulos de una semana sumando los anteriores de lunes a domingo.
@@ -21,6 +21,8 @@ Esto se hace por que el programa hdflitter no puede contar acumulos que implique
 import sys
 import os
 import shutil
+import json
+from collections import OrderedDict
 import datetime
 from datetime import timedelta
 import psycopg2
@@ -28,29 +30,16 @@ from shapely.geometry import shape
 from shapely.geometry import Point
 from shapely import wkt
 
-from read_lag import HDF
 
-
-def conexion():
+def conexion(db_json):
     """
-    Conectase a base de datos do posGIS, e devolve a conexión
+    Connect to a db and return the conection, con
+    :param db_json: str, json file with db parameters to connect to
     :return: con, conexión activa
     """
 
-    # WARNING:
-    #
-    #           Production server host: svr_ide_1
-    #           Developing server host: svr_dev_1
-    #
-    # CHANGE IF NEEDED
-
-    database_data = {'host': 'svr_dev_1',
-                     'port': '5432',
-                     'dbname': 'CleanAtlantic',
-                     'user': 'postgres',
-                     'password': '986512320'}
-
-    # End static configuration
+    with open(db_json, 'r') as f:
+        database_data = json.load(f, object_pairs_hook=OrderedDict)
 
     # Connection to postgis
     connection_string = 'host={0} port={1} dbname={2} user={3} password={4}'.format(database_data['host'],
@@ -208,111 +197,6 @@ class Partic:
         return result
 
 
-def proceso(lag_file_full, data_inicio, data_fin, dt, orixe_name, buffer_name):
-    """
-    Le un ficheiro lagranxiano e calcula a cantidade acumulada en 24 horas por buffer.
-    Os datos están incluido no código na sección datos.
-
-    O programa funciona así:
-
-     1. Le do ficheiro lagranxiano as partículas que fixeron beaching: le o lat, lon e idade (age) da partícula
-        cando fixo beaching.
-     2. Crea unha lista cos intervalos nos que vai contar partículas. Ex. Se o ficheiro é de 10 días e o intervalo
-        dt = 24 (horas), creará unha lista cos datetimes do inicio de cada día.
-     3. Mete todas as partículas lidas nunha lista de obxectos da clase Partic que xa ten a idade, latitude e lonxitude,
-        un obxecto Point, e o intervalo e índice do intervalo correspondente.
-     4. Le os buffers da base de datos e engádelle a cada polígono do buffer, unha lista de cantidades inicializadas
-        a 0. Cada elemento da lista corresponde a cada un dos intervalos temporales onde se vai a calcular a cantidade.
-     5. Percorre todas as particulas e busca se está dentro dun polígono. Se é así, suma a conta de cantidades
-     6. Enche a base de datos con estas cantidades
-
-    :return:
-    """
-    lag_file = HDF(lag_file_full)
-
-    data_fin_intervalos = data_fin
-
-    age_pts = lag_file.get_beachlitter('Age')
-    lat_pts = lag_file.get_beachlitter('Latitude')
-    lon_pts = lag_file.get_beachlitter('Longitude')
-    beach_time_2D = lag_file.get_beachlitter('Beach_Time')
-    dy, n_partic = beach_time_2D.shape
-    beach_time_pts = []
-    for n in range(n_partic):
-        year = beach_time_2D[0][n]
-        month = beach_time_2D[1][n]
-        day = beach_time_2D[2][n]
-        hour = beach_time_2D[3][n]
-        minute = beach_time_2D[4][n]
-        second = beach_time_2D[5][n]
-        beach_time_pt = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
-        beach_time_pts.append(beach_time_pt)
-
-    # Crease unha lista coas datas dos intervalos, identificadas por a data do comezo do intervalo
-    # data_inicio = lag_file.get_times()[spin-1]
-    # data_fin = lag_file.get_times()[-1]
-    # data_fin_intervalos = lag_file.get_times()[-2]
-    print(f'As datas van desde {data_inicio} a {data_fin_intervalos}\n')
-    print(f'A última data dos intervalos será {data_fin_intervalos}\n')
-
-    data_intervalos = []
-    data = data_inicio
-    data_intervalos.append(data)
-    while data < data_fin_intervalos:
-        data += datetime.timedelta(hours=dt)
-        data_intervalos.append(data)
-    print(f'Os intervalos son {data_intervalos}')
-    len_intervalos = len(data_intervalos)
-
-    # Metemos todas as particulas nunha lista de obxectos Partic
-    particulas = []
-    for n, age in enumerate(age_pts):
-        try:
-            particula = Partic(lat_pts[n], lon_pts[n], age_pts[n], beach_time_pts[n])
-            particula.intervalo = particula.get_interval(data_inicio, dt)
-            particula.id_intervalo = data_intervalos.index(particula.intervalo)
-            particulas.append(particula)
-        except ValueError:
-            pass
-
-    # Lemos os poligonos da base de datos
-
-    con = conexion()
-    id_orixe = orixe(con, orixe_name)
-    buffer = Buffer(con, buffer_name)
-    buffer.fill_poligons(con)
-    buffer.add_cantidades_to_poligons(len_intervalos)
-
-    print('O número de particulas é: ', len(particulas))
-    for particula in particulas:
-        # start_time = time()
-        # data_str = data.strftime("%Y-%m-%dT%H:%M:%S")
-        # intervalo = particula.get_interval(data_inicio, dt)
-        # id_intervalo = data_intervalos.index(intervalo)
-        # print(f' Hay unha particula en ({particula.lat},{particula.lon})
-        # con idade {particula.age}: {data_str}, pertence a {id_intervalo}')
-        # particula_pt = Point(particula.lon, particula.lat)
-        for poligono in buffer.poligons:
-            if particula.pt.within(poligono.polygon):
-                poligono.cantidades[particula.id_intervalo] += 1
-        # elapsed_time = time()-start_time
-        # print(f'Elapsed time: {elapsed_time} seconds')
-
-    cur = con.cursor()
-    for poligono in buffer.poligons:
-        for n, data_intervalo in enumerate(data_intervalos):
-            # print(poligono.id, id_orixe, data_intervalo, dt, poligono.cantidades[n])
-
-            sql = '''INSERT INTO  acumulos.cantidade( id_poligono, id_orixe, data, tempo, cantidade)
-                                     VALUES (%s, %s, %s, %s, %s)'''
-            params = (poligono.id, id_orixe, data_intervalo, dt, poligono.cantidades[n],)
-            cur.execute(sql, params)
-            con.commit()
-    print('acabo e pecho')
-    cur.close()
-    con.close()
-
-
 def main():
     """
     vai percorrendo as datas dos distintos ficheiros
@@ -325,10 +209,11 @@ def main():
     orixe_name_fin = 'MohidLitter_01_semanal'
     # buffer_name = 'praias_arousa'
     buffer_name = 'salvora_500_model'
+    db_con = '../datos/db_data.json'
     # FIN DATOS
 
     # Lemos os poligonos da base de datos
-    con = conexion()
+    con = conexion(db_con)
     id_orixe = orixe(con, orixe_name_ini)
     id_orixe_fin = orixe(con, orixe_name_fin)
     buffer = Buffer(con, buffer_name)
@@ -345,7 +230,7 @@ def main():
         con.commit()
 
         resposta = cur.fetchall()
-        print(resposta[3],poligon.id)
+        print(resposta[3], poligon.id)
 
         cant_total = 0
         tempo_total = 0
@@ -356,9 +241,9 @@ def main():
                 parametros = (poligon.id,id_orixe_fin, data, tempo_total, cant_total,)
                 sql = '''INSERT INTO  acumulos.cantidade(id_poligono, id_orixe, data, tempo, cantidade)
                                                      VALUES (%s, %s, %s, %s, %s)'''
-                #print(parametros)
-                cur.execute(sql, parametros)
-                con.commit()
+                print(parametros)
+                #cur.execute(sql, parametros)
+                #con.commit()
 
                 tempo_total = 0
                 cant_total = 0

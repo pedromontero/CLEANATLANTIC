18d4a396410aed3bad3071214b6ea7188fa61ce9
Pedro Montero <pmontero@intecmar.gal>
2020-12-15 12:29:41 +0100

    conexion() and orixe() belongs to cleanatlantic.db module

--- sumlitter/sumlitter.py
+++ sumlitter/sumlitter.py
@@ -4,17 +4,41 @@
 """
 **sumlitter.py**
 
-* *Purpose:* Crea nuevos registros de acumulación, pero ahora a partir de los anteriores porque agrega días. Ejemplo:
-De los acumulos de 24 horas, crea otra lista de acumulos de una semana sumando los anteriores de lunes a domingo.
-Esto se hace por que el programa hdflitter no puede contar acumulos que impliquen más de un fichero lagrangiano.
+* *Purpose:* Create a new list of acumulations in the database, but in this case, it is build by the sum of
+of a number of rows of other list.
+
+* *Description:* sumlitter reads all the amount of beached particles of a buffer from the CleanAtlantic acumulos
+data base and create another origin with the sum of the n times amounts.
+
+The recorded datetime is the end date. All the counted amount is the amount pilled up until that date.
+
+A sumlitter.json is needed:
+
+  "input_origin": original buffer where the amounts will be counted from
+  "output_origin":  the output origin
+  "buffer": the buffer to be counted; It is the same from input and output
+  "sum_rows": number of rows (days if original amounts are 24-hours counting
+  "db_con" : a json file with the parameters of the db connection
+
+  ex:
+
+        {
+        "input_origin": "MohidLitter_01",
+        "output_origin":  "MohidLitter_01_7days",
+        "buffer": "salvora_500_model",
+        "sum_rows": 7,
+        "db_con" : "../datos/db_data.json"
+        }
+
+* *Funding:* CleanAtlantic
 
 * *python version:* 3.7
 * *author:* Pedro Montero
-* *license:* INTECMAR
+* *license:* INTECMAR, CleanAtlantic
 * *requires:* psycopg2
 * *date:* 2020/04/06
-* *version:* 0.0.1
-* *date version* 2020/04/06
+* *version:* 1.0.0
+* *date version* 2020/12/15
 
 """
 
@@ -23,70 +47,10 @@ import json
 from collections import OrderedDict
 import psycopg2
 from cleanatlantic.buffer import Buffer
+from cleanatlantic.db import orixe, conexion
 
 
-def conexion(db_json):
-    """
-    Connect to a db and return the conection, con
-    :param db_json: str, json file with db parameters to connect to
-    :return: con, conexión activa
-    """
 
-    with open(db_json, 'r') as f:
-        database_data = json.load(f, object_pairs_hook=OrderedDict)
-
-    # Connection to postgis
-    connection_string = 'host={0} port={1} dbname={2} user={3} password={4}'.format(database_data['host'],
-                                                                                    database_data['port'],
-                                                                                    database_data['dbname'],
-                                                                                    database_data['user'],
-                                                                                    database_data['password'])
-    try:
-        conn = psycopg2.connect(connection_string)
-    except psycopg2.OperationalError:
-        print('CAUTION: ERROR WHEN CONNECTING TO {0}'.format(database_data['host']))
-        sys.exit()
-    return conn
-
-
-def orixe(conn, orixe_name):
-    """
-    Devolve o id_orixe da conexion con nome orixe_name. Se non existe este orixe, creao.
-
-    :param conn: conexion a base de datos
-    :param orixe_name: nome da orixe (simulacion)
-    :return: id da orixe na base de datos
-    """
-    try:
-        cur = conn.cursor()
-        sql = '''SELECT * FROM acumulos.orixes WHERE tipo=%s'''
-        params = (orixe_name,)
-        cur.execute(sql, params)
-        conn.commit()
-        existe = bool(cur.rowcount)
-    except psycopg2.Error as e:
-        print(' Eror tratando de coenctar a base de datos')
-        print(e)
-
-    # Se non existe inserta
-    try:
-        if not existe:
-            print(f'Vou insertar a seguinte orixe {orixe_name}')
-            sql = '''INSERT INTO acumulos.orixes (tipo)  VALUES (%s)'''
-            params = (orixe_name,)
-            cur.execute(sql, params)
-            conn.commit()
-
-    except psycopg2.IntegrityError as e:
-        print(f'Error ao intentar insertar un orixe con nome {orixe_name}\n')
-        print(e)
-
-    # Selecciona o id da orixe.
-    cur = conn.cursor()
-    cur.execute("select id from acumulos.orixes where tipo=%s", (orixe_name,))
-    ids = cur.fetchall()
-    id_orixe = ids[0][0]
-    return id_orixe
 
 def main():
     """
@@ -142,9 +106,9 @@ def main():
                 parametros = (poligon.id, id_orixe_fin, date_end, sum_time, sum_amount,)
                 sql = '''INSERT INTO  acumulos.cantidade(id_poligono, id_orixe, data, tempo, cantidade)
                                                      VALUES (%s, %s, %s, %s, %s)'''
+
                 cur.execute(sql, parametros)
                 con.commit()
-
 
     cur.close()
     con.close()
